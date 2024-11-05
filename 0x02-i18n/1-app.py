75d88aea516c5d7fc23d80ef9e0608129cf86ecd
be-great <begreat5333@gmail.com>
2024-11-05 13:55:57 +0300

    update

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
- 1. Basic Babel setup
+A Basic flask application
 """
 from flask import Flask
 from flask import render_template
@@ -16,16 +16,21 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# Instantiate the application object
 app = Flask(__name__)
 app.config.from_object(Config)
-Babel = Babel(app)
+
+# Wrap the application with Babel
+babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
-    """render the 1-index page"""
+    """
+    Renders a basic html template
+    """
     return render_template('1-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
