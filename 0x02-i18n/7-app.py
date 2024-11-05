9655c5aeac7f3595e402813d13a6d4241f667a1c
be-great <begreat5333@gmail.com>
2024-11-06 01:22:29 +0300

    task 7

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""
+ 2. Get locale from request
+"""
+from flask import Flask, request
+from flask import render_template, g
+from flask_babel import Babel
+from typing import (
+    Dict, Union
+)
+import pytz
+
+
+class Config(object):
+    """
+    configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale
+    """
+    # add options from where to chose the preference
+    # language
+    options = [
+        request.args.get('locale', '').strip(),
+        g.user.get('locale', None) if g.user else None,
+        request.accept_languages.best_match(app.config['LANGUAGES']),
+        Config.BABEL_DEFAULT_LOCALE
+    ]
+    for locale in options:
+        if locale and locale in Config.LANGUAGES:
+            return locale
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
+    """
+    validate user login details
+    """
+    return users.get(int(id), 0)
+
+
+@babel.timezoneselector
+def get_timezone() -> str:
+    """
+    Gets timezone from request object
+    """
+    timezone = request.args.get('timezone', '').strip()
+    if not timezone and g.user:
+        timezone = g.user['timezone']
+    try:
+        return pytz.timezone(timezone).zone
+    except pytz.exceptions.UnknownTimeZoneError:
+        return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+@app.before_request
+def before_request():
+    """
+    adds valid user to the global session object `g`
+    """
+    setattr(g, 'user', get_user(request.args.get('login_as', 0)))
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """render the 5-index page"""
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
